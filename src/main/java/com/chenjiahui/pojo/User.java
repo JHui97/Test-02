bea92de5e28863ceb7293bd4330245358b5692a9
JHui97 <1174005513@qq.com>
2022-02-27 23:02:49 +0800

    init

---
+++ src/main/java/com/chenjiahui/pojo/User.java
@@ -0,0 +1,12 @@
+package com.chenjiahui.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @Author: ChenJH
+ * @Date: 2022/2/27 22:52
+ */
+public class User implements Serializable {
+    String name;
+    Integer age;
+}
